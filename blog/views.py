37d10c2e6210b6fec15516fdebd5d9411e0bc3f0
fomid8 <fati.omid2000@gmail.com>
2024-11-07 16:46:33 +0330

    add base views for user, category and article

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import mixins
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
