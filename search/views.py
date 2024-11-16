676a0252ce7c56e8d879f83589294e0eb08b9acf
fomid8 <fati.omid2000@gmail.com>
2024-11-17 01:15:50 +0330

    add preprocess for search

--- search/views.py
+++ search/views.py
@@ -6,6 +6,9 @@ from rest_framework import views
 from rest_framework.pagination import LimitOffsetPagination
 from blog.documents import ArticleDocument, UserDocument, CategoryDocument
 from blog.serializers import ArticleSerializer, UserSerializer, CategorySerializer
+import re
+import unicodedata
+
 
 class PaginatedElasticSearchAPIView(views.APIView, LimitOffsetPagination):
     serializer_class = None
@@ -16,14 +19,22 @@ class PaginatedElasticSearchAPIView(views.APIView, LimitOffsetPagination):
         """This method should be overridden
         and return a Q() expression."""
 
+    def preprocess_query(self, query):
+        # remove extra spaces
+        query = query.strip()
+        # query = query.lower()
+        query = unicodedata.normalize('NFKC', query)
+        # remove extra characters
+        query = re.sub(r'[^\w\s]', '', query)
+        return query
+
     def get(self, request, query):
         try:
-            q = self.generate_q_expression(query)
+            preprocessed_query = self.preprocess_query(query)
+            q = self.generate_q_expression(preprocessed_query)
             search = self.document_class.search().query(q)
             response = search.execute()
-
             print(f"Found {response.hits.total.value} hit(s) for query: '{query}'")
-
             results = self.paginate_queryset(response, request, view=self)
             serializer = self.serializer_class(results, many=True)
             return self.get_paginated_response(serializer.data)
@@ -41,6 +52,8 @@ class SearchUsers(PaginatedElasticSearchAPIView):
                      Q("match", username=query),
                      Q("match", first_name=query),
                      Q("match", last_name=query),
+                     # Ngram
+                     Q("query_string", query=f"*{query}*", fields=["username^2", "first_name^1", "last_name^1"])
                  ], minimum_should_match=1)
 
 
@@ -68,8 +81,8 @@ class SearchArticles(PaginatedElasticSearchAPIView):
                     "title",
                     "category"
                 ], fuzziness="auto")
-    
-    
+
+
 class SearchArticlesByAuthor(PaginatedElasticSearchAPIView):
     serializer_class = ArticleSerializer
     document_class = ArticleDocument
